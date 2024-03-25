9643cbfe29bf0c36c3cdece978b3c7a13d13c90c
bitcamp <bitcamp@bit>
2024-03-25 13:18:12 +0900

    master : 2024-03-25 : restHttpClientApp

--- 08.Model2MVCShop(RestFul Client)/src/client/app/RestHttpClientApp.java
+++ 08.Model2MVCShop(RestFul Client)/src/client/app/RestHttpClientApp.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -363,9 +364,13 @@ public class RestHttpClientApp {
 		JSONObject jsonobj = (JSONObject)JSONValue.parse(br);
 		System.out.println(jsonobj + "jsonobj");
 	
+ 
+		
 		ObjectMapper objectMapper = new ObjectMapper();
-		List list = objectMapper.readValue(jsonobj.toString(), List.class);
-		System.out.println(list +"product");
+		
+		Map<String, Object> map = objectMapper.readValue(jsonobj.toString(), Map.class);
+//		List list = objectMapper.readValue(jsonobj.toString(), List.class);
+		System.out.println(map +"product");
 	}
 	
 	
